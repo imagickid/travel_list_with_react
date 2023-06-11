6a9cac429a753f5ee4b2439ad530893742b94433
AJ Djiemuratov <lmt9229@yahoo.com>
2023-06-11 22:44:50 +0500

    travel_list_react

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import Logo from "./Logo";
+import Form from "./Form";
+import { useState } from "react";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: true },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+// ];
+
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  const handleAddItems = item => {
+    setItems(items => [...items, item]);
+  };
+
+  const handleDeleteItem = id => {
+    setItems(items => items.filter(item => item.id !== id));
+  };
+
+  const handleToggleItem = id => {
+    setItems(items =>
+      items.map(item =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  };
+
+  const handleClearList = () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  };
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+};
+
+export default App;
